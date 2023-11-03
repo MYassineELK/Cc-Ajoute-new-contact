979415e7bf19780175dc4da543b3c22d15fe84d0
MYassineELK <elkyassin41@gamil.com>
2023-11-03 15:47:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Contact from './compsont/Contact'
+import Accueil from './compsont/Accueil'
+
+export default function App() {
+  return (
+    <div>
+      <Accueil/>
+    </div>
+  )
+}
